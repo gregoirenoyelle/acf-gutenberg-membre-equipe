8b1b3a4a6fb612cfa9e66f61a5161a0babe6defd
Grégoire Noyelle <hello@gregoirenoyelle.com>
2019-01-04 17:01:26 +0100

    Ajout des références supplémentaire: site et email

--- view/view-bloc-equipe.php
+++ view/view-bloc-equipe.php
@@ -8,4 +8,11 @@
 	<div class="membre-bio">
 		<?php echo wp_kses_post($bio) ?>
 	</div>
+	<div class="membre-meta">
+		<h3>Références supplémentaires</h3>
+		<ul>
+			<li><a href="<?php echo esc_url( $site ); ?>">Visiter le site internet</a></li>
+			<li><a href="mailto:<?php echo esc_html( antispambot($email) ); ?>">Envoyer un message</a></li>
+		</ul>
+	</div>
 </section>

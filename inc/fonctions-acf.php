3b2aa4914f1b5334069a31b1e3411a46da0e879d
Grégoire Noyelle <hello@gregoirenoyelle.com>
2019-06-30 15:35:37 +0200

    * Ajout des données ACF pour la boucle
    Pour l'instant le texte ne s'affiche pas.

--- inc/fonctions-acf.php
+++ inc/fonctions-acf.php
@@ -2,60 +2,53 @@
 /**
  * Fonction pour la création de blocs Gutenberg avec ACF
  *
- * @package acf-bloc-membres
+ * @package acf-bloc-gncom
  * @author Grégoire Noyelle
  * @since 1.0.0
  * @link https://www.advancedcustomfields.com/resources/acf_register_block/
  */
-function acf_bloc_gutenberg_equipe() {
-	
-	/* Vérifie si c'est la bonne version ACF ou si ACF est actif. 
-		 Dans le cas contraire tout s'arrête. 
+function acf_bloc_gutenberg_temoignages() {
+
+	/* Vérifie si c'est la bonne version ACF ou si ACF est actif.
+		 Dans le cas contraire tout s'arrête.
 	*/
 	if ( ! function_exists('acf_register_block') )
 		return;
-	
+
 	//* Enregistrement du bloc
 	acf_register_block(array(
-		'name' => 'membre-equipe',
-		'title' => 'Membres de l\'équipe',
-		'render_callback' => 'acf_bloc_gutenberg_equipe_callback',
+		'name' => 'grille-temoignage',
+		'title' => 'Grille de témoignage',
+		'render_callback' => 'acf_bloc_gutenberg_temoignages_callback',
 		'category' => 'formatting',
-		'icon' => 'admin-users',
+		'icon' => 'dashicons-thumbs-up',
 		'mode' => 'edit',
 		'post_types' => array('page'),
 		'keywords' => array('profile', 'user', 'author')
 	));
 }
-add_action('acf/init', 'acf_bloc_gutenberg_equipe');
+add_action('acf/init', 'acf_bloc_gutenberg_temoignages');
 
 
 
 /**
  * Fonction de Callback pour l'affichage des blocs Gutenberg avec ACF
- * 
- * Appelée depuis la fonction acf_bloc_gutenberg_equipe()
  *
- * @package acf-bloc-membres
+ *
+ * @package acf-bloc-gncom
  * @author Grégoire Noyelle
  * @since 1.0.0
  */
-function acf_bloc_gutenberg_equipe_callback() {
-	
+function acf_bloc_gutenberg_temoignages_callback() {
+
 	//* Variables ACF
-	$prenom = get_field('blocequipe_prenom');
-	$nom = get_field('blocequipe_nom');
-	$bio = get_field('blocequipe_bio');
-	$img_id = (int) get_field('blocequipe_photo');
-	$img = wp_get_attachment_image( $img_id, 'cuisinier' );
-	$email = get_field('blocequipe_email');
-	$site = get_field('blocequipe_site');
-	$back_color = get_field('blocequipe_arriere_plan');
-	$padding = get_field('blocequipe_marge_interne');
+	$type = get_field('bloc_temoi_type_temoignages');
+	$nombre = get_field('bloc_temoi_nombre_temoignages');
+	$lien = get_field('bloc_temoi_lien_bouton');
 
 	//* Appel du fichier pour l'affichage HTML
 	ob_start();
-	include plugin_dir_path(__DIR__) . 'view/view-bloc-equipe.php';
+	include plugin_dir_path(__DIR__) . 'view/view-bloc-temoignages.php';
 	// Affichage et nettoyage de la mémoire tampon
 	echo ob_get_clean();
 }

e896b5edafb822da7e201b133e7535472f66b83d
Grégoire Noyelle <hello@gregoirenoyelle.com>
2021-03-22 15:36:05 +0100

    Modification des URL

--- acf-gutenberg-blocs.php
+++ acf-gutenberg-blocs.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: ACF Gutenberg Blocs
-Plugin URI: https://wwww.gregoirenoyelle.com
+Plugin URI: https://www.gregoirenoyelle.com
 Description: Exemple d'extension avec ACF pour l'éditeur moderne (Gutenberg).
 Version: 1.1.0
 Author: Grégoire Noyelle
-Author URI: http://wwww.gregoirenoyelle.com
+Author URI: http://www.gregoirenoyelle.com
 License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Domain Path: /languages
